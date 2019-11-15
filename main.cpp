e80a40bd644173f8e128b8ec6654438dbf3dae73
Justin Adams <justin.adams@uvm.edu>
2019-11-15 16:05:08 -0500

    Uncommenting sections of code.

--- main.cpp
+++ main.cpp
@@ -59,178 +59,178 @@ int main() {
 
     /** Generate run time data for Bubble Sort with different numbers of objects. */
     while(count < max) {
-//        /** Create a vector to store random numbers. */
-//        randomOrder = algs->randomIntegerVector(0, appCount-1);
-//        /** Clear the unordered apps vector. */
-//        unorderedApps->clear();
-//        /** Rearrange the objects in the vector based on the new arrangement. */
-//        #pragma omp parallel for
-//        for (int i = 0; i < count; ++i) {
-//            /** Push back the randomly selected application. */
-//            unorderedApps->push_back(orderedApps->at(randomOrder->at(i)));
-//        }
-//        /** Output count. */
-//        cout << "Bubble Sort is sorting " << count << " objects." << endl;
-//        /** Test bubble sort algorithm. */
-//        algorithms->bubbleSort(*unorderedApps);
-//        /** Store the number of reads. */
-//        reads = algorithms->getReads();
-//        /** Store the number of writes. */
-//        writes = algorithms->getWrites();
-//        /** Store the run time duration. */
-//        runTime = algorithms->calculateTimeSpan();
-//        /** Write the data to file. */
-//        file << "Bubble Sort, " << count << ", " << reads << ", " << writes << ", " << runTime.count() << endl;
-//        /** Reset the read count. */
-//        algorithms->setReads(0);
-//        /** Reset the write count. */
-//        algorithms->setWrites(0);
-//        /** Increment count. */
-//        count += interval;
-//    }
-//
-//    /** Reset the count value. */
-//    count = 100;
-//
-//    /** Generate run time data for Selection Sort with different numbers of objects. */
-//    while(count < max) {
-//        /** Create a vector to store random numbers. */
-//        randomOrder = algs->randomIntegerVector(0, appCount-1);
-//        /** Clear the unordered apps vector. */
-//        unorderedApps->clear();
-//        /** Rearrange the objects in the vector based on the new arrangement. */
-//        #pragma omp parallel for
-//        for (int i = 0; i < count; ++i) {
-//            /** Push back the randomly selected application. */
-//            unorderedApps->push_back(orderedApps->at(randomOrder->at(i)));
-//        }
-//        /** Output count. */
-//        cout << "Selection sort is sorting " << count << " objects." << endl;
-//        /** Test selection sort algorithm. */
-//        algorithms->selectionSort(*unorderedApps);
-//        /** Store the number of reads. */
-//        reads = algorithms->getReads();
-//        /** Store the number of writes. */
-//        writes = algorithms->getWrites();
-//        /** Store the run time duration. */
-//        runTime = algorithms->calculateTimeSpan();
-//        /** Write the data to file. */
-//        file << "Selection Sort, " << count << ", " << reads << ", " << writes << ", " << runTime.count() << endl;
-//        /** Reset the read count. */
-//        algorithms->setReads(0);
-//        /** Reset the write count. */
-//        algorithms->setWrites(0);
-//        /** Increment count. */
-//        count += interval;
-//    }
-//
-//    /** Reset the count value. */
-//    count = 100;
-//
-//    /** Generate run time data for Insertion Sort with different numbers of objects. */
-//    while(count < max) {
-//        /** Create a vector to store random numbers. */
-//        randomOrder = algs->randomIntegerVector(0, appCount-1);
-//        /** Clear the unordered apps vector. */
-//        unorderedApps->clear();
-//        /** Rearrange the objects in the vector based on the new arrangement. */
-//        #pragma omp parallel for
-//        for (int i = 0; i < count; ++i) {
-//            /** Push back the randomly selected application. */
-//            unorderedApps->push_back(orderedApps->at(randomOrder->at(i)));
-//        }
-//        /** Output count. */
-//        cout << "Insertion sort is sorting " << count << " objects." << endl;
-//        /** Test insertion sort algorithm. */
-//        algorithms->insertionSort(*unorderedApps);
-//        /** Store the number of reads. */
-//        reads = algorithms->getReads();
-//        /** Store the number of writes. */
-//        writes = algorithms->getWrites();
-//        /** Store the run time duration. */
-//        runTime = algorithms->calculateTimeSpan();
-//        /** Write the data to file. */
-//        file << "Insertion Sort, " << count << ", " << reads << ", " << writes << ", " << runTime.count() << endl;
-//        /** Reset the read count. */
-//        algorithms->setReads(0);
-//        /** Reset the write count. */
-//        algorithms->setWrites(0);
-//        /** Increment count. */
-//        count += interval;
-//    }
-//
-//    /** Reset the count value. */
-//    count = 100;
-//
-//    /** Generate run time data for Merge Sort with different numbers of objects. */
-//    while(count < max) {
-//        /** Create a vector to store random numbers. */
-//        randomOrder = algs->randomIntegerVector(0, appCount-1);
-//        /** Clear the unordered apps vector. */
-//        unorderedApps->clear();
-//        /** Rearrange the objects in the vector based on the new arrangement. */
-//        #pragma omp parallel for
-//        for (int i = 0; i < count; ++i) {
-//            /** Push back the randomly selected application. */
-//            unorderedApps->push_back(orderedApps->at(randomOrder->at(i)));
-//        }
-//        /** Output count. */
-//        cout << "Merge sort is sorting " << count << " objects." << endl;
-//        /** Test merge sort algorithm. */
-//        algorithms->mergeSort(*unorderedApps, 0, int(unorderedApps->size()) - 1);
-//        /** Store the number of reads. */
-//        reads = algorithms->getReads();
-//        /** Store the number of writes. */
-//        writes = algorithms->getWrites();
-//        /** Store the run time duration. */
-//        runTime = algorithms->calculateTimeSpan();
-//        /** Write the data to file. */
-//        file << "Merge Sort, " << count << ", " << reads << ", " << writes << ", " << runTime.count() << endl;
-//        /** Reset the read count. */
-//        algorithms->setReads(0);
-//        /** Reset the write count. */
-//        algorithms->setWrites(0);
-//        /** Increment count. */
-//        count += interval;
-//    }
-//
-//    /** Reset the count value. */
-//    count = 100;
-//
-//    /** Generate run time data for Quick Sort with different numbers of objects. */
-//    while(count < max) {
-//        /** Create a vector to store random numbers. */
-//        randomOrder = algs->randomIntegerVector(0, appCount-1);
-//        /** Clear the unordered apps vector. */
-//        unorderedApps->clear();
-//        /** Rearrange the objects in the vector based on the new arrangement. */
-//        #pragma omp parallel for
-//        for (int i = 0; i < count; ++i) {
-//            /** Push back the randomly selected application. */
-//            unorderedApps->push_back(orderedApps->at(randomOrder->at(i)));
-//        }
-//        /** Output count. */
-//        cout << "Quick sort is sorting " << count << " objects." << endl;
-//        /** Test quick sort algorithm. */
-//        algorithms->quickSort(*unorderedApps);
-//        /** Store the number of reads. */
-//        reads = algorithms->getReads();
-//        /** Store the number of writes. */
-//        writes = algorithms->getWrites();
-//        /** Store the run time duration. */
-//        runTime = algorithms->calculateTimeSpan();
-//        /** Write the data to file. */
-//        file << "Quick Sort, " << count << ", " << reads << ", " << writes << ", " << runTime.count() << endl;
-//        /** Reset the read count. */
-//        algorithms->setReads(0);
-//        /** Reset the write count. */
-//        algorithms->setWrites(0);
-//        /** Increment count. */
-//        count += interval;
-//    }
-//
-//    /** Reset the count value. */
-//    count = 100;
+        /** Create a vector to store random numbers. */
+        randomOrder = algs->randomIntegerVector(0, appCount-1);
+        /** Clear the unordered apps vector. */
+        unorderedApps->clear();
+        /** Rearrange the objects in the vector based on the new arrangement. */
+        #pragma omp parallel for
+        for (int i = 0; i < count; ++i) {
+            /** Push back the randomly selected application. */
+            unorderedApps->push_back(orderedApps->at(randomOrder->at(i)));
+        }
+        /** Output count. */
+        cout << "Bubble Sort is sorting " << count << " objects." << endl;
+        /** Test bubble sort algorithm. */
+        algorithms->bubbleSort(*unorderedApps);
+        /** Store the number of reads. */
+        reads = algorithms->getReads();
+        /** Store the number of writes. */
+        writes = algorithms->getWrites();
+        /** Store the run time duration. */
+        runTime = algorithms->calculateTimeSpan();
+        /** Write the data to file. */
+        file << "Bubble Sort, " << count << ", " << reads << ", " << writes << ", " << runTime.count() << endl;
+        /** Reset the read count. */
+        algorithms->setReads(0);
+        /** Reset the write count. */
+        algorithms->setWrites(0);
+        /** Increment count. */
+        count += interval;
+    }
+
+    /** Reset the count value. */
+    count = 100;
+
+    /** Generate run time data for Selection Sort with different numbers of objects. */
+    while(count < max) {
+        /** Create a vector to store random numbers. */
+        randomOrder = algs->randomIntegerVector(0, appCount-1);
+        /** Clear the unordered apps vector. */
+        unorderedApps->clear();
+        /** Rearrange the objects in the vector based on the new arrangement. */
+        #pragma omp parallel for
+        for (int i = 0; i < count; ++i) {
+            /** Push back the randomly selected application. */
+            unorderedApps->push_back(orderedApps->at(randomOrder->at(i)));
+        }
+        /** Output count. */
+        cout << "Selection sort is sorting " << count << " objects." << endl;
+        /** Test selection sort algorithm. */
+        algorithms->selectionSort(*unorderedApps);
+        /** Store the number of reads. */
+        reads = algorithms->getReads();
+        /** Store the number of writes. */
+        writes = algorithms->getWrites();
+        /** Store the run time duration. */
+        runTime = algorithms->calculateTimeSpan();
+        /** Write the data to file. */
+        file << "Selection Sort, " << count << ", " << reads << ", " << writes << ", " << runTime.count() << endl;
+        /** Reset the read count. */
+        algorithms->setReads(0);
+        /** Reset the write count. */
+        algorithms->setWrites(0);
+        /** Increment count. */
+        count += interval;
+    }
+
+    /** Reset the count value. */
+    count = 100;
+
+    /** Generate run time data for Insertion Sort with different numbers of objects. */
+    while(count < max) {
+        /** Create a vector to store random numbers. */
+        randomOrder = algs->randomIntegerVector(0, appCount-1);
+        /** Clear the unordered apps vector. */
+        unorderedApps->clear();
+        /** Rearrange the objects in the vector based on the new arrangement. */
+        #pragma omp parallel for
+        for (int i = 0; i < count; ++i) {
+            /** Push back the randomly selected application. */
+            unorderedApps->push_back(orderedApps->at(randomOrder->at(i)));
+        }
+        /** Output count. */
+        cout << "Insertion sort is sorting " << count << " objects." << endl;
+        /** Test insertion sort algorithm. */
+        algorithms->insertionSort(*unorderedApps);
+        /** Store the number of reads. */
+        reads = algorithms->getReads();
+        /** Store the number of writes. */
+        writes = algorithms->getWrites();
+        /** Store the run time duration. */
+        runTime = algorithms->calculateTimeSpan();
+        /** Write the data to file. */
+        file << "Insertion Sort, " << count << ", " << reads << ", " << writes << ", " << runTime.count() << endl;
+        /** Reset the read count. */
+        algorithms->setReads(0);
+        /** Reset the write count. */
+        algorithms->setWrites(0);
+        /** Increment count. */
+        count += interval;
+    }
+
+    /** Reset the count value. */
+    count = 100;
+
+    /** Generate run time data for Merge Sort with different numbers of objects. */
+    while(count < max) {
+        /** Create a vector to store random numbers. */
+        randomOrder = algs->randomIntegerVector(0, appCount-1);
+        /** Clear the unordered apps vector. */
+        unorderedApps->clear();
+        /** Rearrange the objects in the vector based on the new arrangement. */
+        #pragma omp parallel for
+        for (int i = 0; i < count; ++i) {
+            /** Push back the randomly selected application. */
+            unorderedApps->push_back(orderedApps->at(randomOrder->at(i)));
+        }
+        /** Output count. */
+        cout << "Merge sort is sorting " << count << " objects." << endl;
+        /** Test merge sort algorithm. */
+        algorithms->mergeSort(*unorderedApps, 0, int(unorderedApps->size()) - 1);
+        /** Store the number of reads. */
+        reads = algorithms->getReads();
+        /** Store the number of writes. */
+        writes = algorithms->getWrites();
+        /** Store the run time duration. */
+        runTime = algorithms->calculateTimeSpan();
+        /** Write the data to file. */
+        file << "Merge Sort, " << count << ", " << reads << ", " << writes << ", " << runTime.count() << endl;
+        /** Reset the read count. */
+        algorithms->setReads(0);
+        /** Reset the write count. */
+        algorithms->setWrites(0);
+        /** Increment count. */
+        count += interval;
+    }
+
+    /** Reset the count value. */
+    count = 100;
+
+    /** Generate run time data for Quick Sort with different numbers of objects. */
+    while(count < max) {
+        /** Create a vector to store random numbers. */
+        randomOrder = algs->randomIntegerVector(0, appCount-1);
+        /** Clear the unordered apps vector. */
+        unorderedApps->clear();
+        /** Rearrange the objects in the vector based on the new arrangement. */
+        #pragma omp parallel for
+        for (int i = 0; i < count; ++i) {
+            /** Push back the randomly selected application. */
+            unorderedApps->push_back(orderedApps->at(randomOrder->at(i)));
+        }
+        /** Output count. */
+        cout << "Quick sort is sorting " << count << " objects." << endl;
+        /** Test quick sort algorithm. */
+        algorithms->quickSort(*unorderedApps);
+        /** Store the number of reads. */
+        reads = algorithms->getReads();
+        /** Store the number of writes. */
+        writes = algorithms->getWrites();
+        /** Store the run time duration. */
+        runTime = algorithms->calculateTimeSpan();
+        /** Write the data to file. */
+        file << "Quick Sort, " << count << ", " << reads << ", " << writes << ", " << runTime.count() << endl;
+        /** Reset the read count. */
+        algorithms->setReads(0);
+        /** Reset the write count. */
+        algorithms->setWrites(0);
+        /** Increment count. */
+        count += interval;
+    }
+
+    /** Reset the count value. */
+    count = 100;
 
     /** Generate run time data for Heap Sort with different numbers of objects. */
     while(count < max) {
@@ -264,39 +264,39 @@ int main() {
         count += interval;
     }
 
-//    /** Reset the count value. */
-//    count = 100;
-//
-//    /** Generate run time data for Two Sort with different numbers of objects. */
-//    while(count < max) {
-//        /** Create a vector to store random numbers. */
-//        randomOrder = algs->randomIntegerVector(0, appCount-1);
-//        /** Clear the unordered apps vector. */
-//        unorderedApps->clear();
-//        /** Rearrange the objects in the vector based on the new arrangement. */
-//        #pragma omp parallel for
-//        for (int i = 0; i < count; ++i) {
-//            /** Push back the randomly selected application. */
-//            unorderedApps->push_back(orderedApps->at(randomOrder->at(i)));
-//        }
-//        /** Output count. */
-//        cout << "Two sort is sorting " << count << " objects." << endl;
-//        /** Test two sort algorithm. */
-//        algorithms->twoSort(*unorderedApps);
-//        /** Store the number of reads. */
-//        reads = algorithms->getReads();
-//        /** Store the number of writes. */
-//        writes = algorithms->getWrites();
-//        /** Store the run time duration. */
-//        runTime = algorithms->calculateTimeSpan();
-//        /** Write the data to file. */
-//        file << "Two Sort, " << count << ", " << reads << ", " << writes << ", " << runTime.count() << endl;
-//        /** Reset the read count. */
-//        algorithms->setReads(0);
-//        /** Reset the write count. */
-//        algorithms->setWrites(0);
-//        /** Increment count. */
-//        count += interval;
+    /** Reset the count value. */
+    count = 100;
+
+    /** Generate run time data for Two Sort with different numbers of objects. */
+    while(count < max) {
+        /** Create a vector to store random numbers. */
+        randomOrder = algs->randomIntegerVector(0, appCount-1);
+        /** Clear the unordered apps vector. */
+        unorderedApps->clear();
+        /** Rearrange the objects in the vector based on the new arrangement. */
+        #pragma omp parallel for
+        for (int i = 0; i < count; ++i) {
+            /** Push back the randomly selected application. */
+            unorderedApps->push_back(orderedApps->at(randomOrder->at(i)));
+        }
+        /** Output count. */
+        cout << "Two sort is sorting " << count << " objects." << endl;
+        /** Test two sort algorithm. */
+        algorithms->twoSort(*unorderedApps);
+        /** Store the number of reads. */
+        reads = algorithms->getReads();
+        /** Store the number of writes. */
+        writes = algorithms->getWrites();
+        /** Store the run time duration. */
+        runTime = algorithms->calculateTimeSpan();
+        /** Write the data to file. */
+        file << "Two Sort, " << count << ", " << reads << ", " << writes << ", " << runTime.count() << endl;
+        /** Reset the read count. */
+        algorithms->setReads(0);
+        /** Reset the write count. */
+        algorithms->setWrites(0);
+        /** Increment count. */
+        count += interval;
     }
     /** Output the values to a file. */
     file.close();
